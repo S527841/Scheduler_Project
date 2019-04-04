dd5c555640d376fd3ad17a7a5948cdce4322a2b0
Logan11999 <32521796+Logan11999@users.noreply.github.com>
2019-04-04 15:50:32 -0500

    made a singelton

--- Bearcat_Scheduler/Bearcat_Scheduler/Professors.swift
+++ Bearcat_Scheduler/Bearcat_Scheduler/Professors.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class Professor {
+    var name:String
+    
+    init(name:String){
+        self.name = name
+    }
+    
+class Major {
+    var name:String
+    var professors:[Professor] = []
+    
+    init(name:String){
+        self.name = name
+    }
+    
+class Majors {
+    private init() {}
+    
+    static var shared = Majors()
+    
+    private var majors:[Majors] = []
+    
+    func numMajors() -> Int {
+        return majors.count
+    }
+    }
+}
+}
