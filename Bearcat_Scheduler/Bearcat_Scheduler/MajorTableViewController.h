82221fa129a3bc5b815ac612a4a60dfc9aec99b0
Smith,Logan W <s524601@ch112013m.nwmissouri.edu>
2019-04-04 15:29:03 -0500

    changes some colors and added another table view

---
+++ Bearcat_Scheduler/Bearcat_Scheduler/MajorTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  MajorTableViewController.h
+//  Bearcat_Scheduler
+//
+//  Created by Smith,Logan W on 4/4/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MajorTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Bearcat_Scheduler/Bearcat_Scheduler/Professors.swift
@@ -0,0 +1,8 @@
+//
+//  Professors.swift
+//  Bearcat_Scheduler
+//
+//  Created by Smith,Logan W on 4/4/19.
+//
+
+import Foundation
