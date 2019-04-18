ad3b53dcb15dbc65fa46c7eac316e773e9f448df
David Chase Moyer <S527841@nwmissouri.edu>
2019-04-18 15:29:11 -0500

    fixed title of view controller, removed unnecessary comments

--- Scheduler_Project-master 3/Bearcat_Scheduler/Bearcat_Scheduler/ProfessorTableViewController.swift
+++ Scheduler_Project-master 3/Bearcat_Scheduler/Bearcat_Scheduler/ProfessorTableViewController.swift
@@ -13,14 +13,7 @@ class ProfessorTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
@@ -28,7 +21,6 @@ class ProfessorTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return Professors.shared.numProfessors()
     }
     
--- Scheduler_Project-master 3/Bearcat_Scheduler/Bearcat_Scheduler/ScheduleViewController.swift
+++ Scheduler_Project-master 3/Bearcat_Scheduler/Bearcat_Scheduler/ScheduleViewController.swift
@@ -14,7 +14,6 @@ class ScheduleViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
